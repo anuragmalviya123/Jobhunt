fa20f61d420e693b704b6db8697713a5ba6c816a
Anurag Malviya <anuragmalviya62@gmail.com>
2024-09-02 08:59:41 +0530

    start frontend

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { login, logout, register, updateProfile } from '../controllers/user.controllers.js';
 import isAuthenticated from '../middlewares/isAuthenticated.js';
+import { singleUpload } from '../middlewares/multer.js';
 
 const router = express.Router();
 
 
-router.route("/register").post(register);
+router.route("/register").post(singleUpload,register);
 router.route("/login").post(login);
 router.route("/Profile/update").post(isAuthenticated, updateProfile);
 router.route("/logout").get(logout);
