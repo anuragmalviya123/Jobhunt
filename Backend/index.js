47ea1febf160baadcb3c9bfd79a8f009671f1da0
Anurag Malviya <anuragmalviya62@gmail.com>
2024-09-07 16:12:07 +0530

    Project done

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
+
 //route
 import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
